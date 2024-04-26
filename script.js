c46a50cdd96edf83f7d6ecb716e950eaaa99f7d0
Ignacio Moya <moyaignacioj@gmail.com>
2024-04-26 14:54:24 -0300

    First draft for daily loading calculation.

---
+++ script.js
@@ -0,0 +1,13 @@
+var btn=document.getElementById("carga-diaria");
+console.log(console);
+btn.addEventListener("click", function(){
+    let result=document.getElementById("result");
+    result.classList.remove("hide");
+});
+
+var btn=document.getElementById("agua-agregada");
+console.log(console);
+btn.addEventListener("click", function(){
+    let result=document.getElementById("result1");
+    result.classList.remove("hide");
+});
